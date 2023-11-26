0f893f3fc8b0c40fde3833aae75c0afc48c22693
Bulat <@LAPTOP-SSS2TI2M>
2023-11-26 23:29:46 +0300

    Task 4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
     //Task 1
-    //    println(Event(title="Study Kotlin", description="Commit to studying Kotlin at least 15 minutes per day.",
-    //        daypart="Evening", durationInMinutes=15))
+//    println(Event(title="Study Kotlin", description="Commit to studying Kotlin at least 15 minutes per day.",
+//        daypart="Evening", durationInMinutes=15))
     //Task 2
     //println(Event(title="Study Kotlin", description="Commit to studying Kotlin at least 15 minutes per day.",
     //    daypart=Daypart.EVENING, durationInMinutes=15))
@@ -12,4 +12,9 @@ fun main(args: Array<String>) {
     val event4 = Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60)
     val event5 = Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10)
     val event6 = Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
-    val events = mutableListOf<Event>(event1, event2, event3, event4, event5, event6)}
+    val events = mutableListOf<Event>(event1, event2, event3, event4, event5, event6)
+
+    //Task 4
+    val shortEvents = events.filter { it.durationInMinutes < 60 }
+    println("You have ${shortEvents.size} short events.")
+}
